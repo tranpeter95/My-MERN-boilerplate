408eb3d286a6dddde6b302afa195ce5a5cd49630
peter <tranpeter1095@gmail.com>
2023-05-20 15:49:37 -0600

    added mongoose and hooked up db. also made .env file to hide important info

--- server/server.js
+++ server/server.js
@@ -1,7 +1,14 @@
+require('dotenv').config(); // uses .env file that is gitignored for port instead of showing port
+
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
 
+// express app
 const app = express();
+
+// always parses json
+app.use(express.json());
 
 // will serve the static assets in build folder in client side, (index.html will be able to use css and bundle.js)
 app.use(express.static(path.resolve(__dirname, '../client/build')));
@@ -11,6 +18,15 @@ app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
 });
 
-app.listen(5000, () => {
-  console.log('listening on Port 5000');
-});
+// connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    //  only want to start listening after connecting to db
+    app.listen(process.env.PORT, () => {
+      console.log('listening on Port', process.env.PORT);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
