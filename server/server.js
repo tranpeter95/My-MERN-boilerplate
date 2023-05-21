560be83dbc117a28091b648ca7672aed23bbe573
peter <tranpeter1095@gmail.com>
2023-05-21 15:13:59 -0600

    added default ports and db for users who fork, along w route folder

--- server/server.js
+++ server/server.js
@@ -1,8 +1,14 @@
-require('dotenv').config(); // uses .env file that is gitignored for port instead of showing port
+// uses .env file that is gitignored for port instead of showing port
+require('dotenv').config();
 
+// various requires
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+
+// will either use secret links in env, or hardcoded for ppl who forked
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost/projectdb';
+const port = process.env.PORT || 2000;
 
 // express app
 const app = express();
@@ -18,13 +24,25 @@ app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
 });
 
+// 404
+app.use('*', (req, res) => {
+  res.status(404).send('Not Found');
+});
+
+// global err handler
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).send({ error: err });
+});
+
 // connect to db
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(MONGO_URI)
   .then(() => {
     //  only want to start listening after connecting to db
-    app.listen(process.env.PORT, () => {
-      console.log('listening on Port', process.env.PORT);
+    app.listen(port, () => {
+      console.log(`listening on port ${port}`);
+      console.log(`using database: ${MONGO_URI}`);
     });
   })
   .catch((err) => {
